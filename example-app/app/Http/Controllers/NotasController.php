7065163d331d6e4f33fd388a351cd7f86d24b63c
RobertRosu <146177835+RobertRosu@users.noreply.github.com>
2024-12-05 10:28:02 +0000

    Endpoint notes creado

--- example-app/app/Http/Controllers/NotasController.php
+++ example-app/app/Http/Controllers/NotasController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Notas;
 
 class NotasController extends Controller
 {
-    //
+    public function index(){
+        return "<h1>Hola mundo</h1>";
+    }
 }
